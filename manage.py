e1c6d27effd67145eb5f08efd4ec64cdd3a3621f
sandrinemfuranzima <75666150+sandrinemfuranzima@users.noreply.github.com>
2021-04-08 10:45:32 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -17,7 +17,6 @@ def test():
     unittest.TextTestRunner(verbosity=2).run(tests)
     
 @manager.shell
-
 def make_shell_context():
     return dict(app = app,db = db,User = User, Blog = Blog, Comment = Comment )
 if __name__ == '__main__':
