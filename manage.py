3a15c4fe2b3c4d53adb07c8a0582f0e1501a45a1
sandrinemfuranzima <75666150+sandrinemfuranzima@users.noreply.github.com>
2021-04-08 10:42:17 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,6 +15,7 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User, Blog = Blog, Comment = Comment )
